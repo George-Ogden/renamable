7b7888e7b21b87312dde5d9d2ff84a0b9839f272
George <george-ogden@outlook.com>
2024-04-29 12:42:38 +0100

    define specific add interface

--- interface.py
+++ interface.py
@@ -1,10 +1,33 @@
 from __future__ import annotations
 
-from typing import Self
+from typing import Self, Type
 
 
-class Add:
+class Interface:
+    _alternative_name: str = "value"
+
+    def __class_getitem__(cls, alternative_name: str) -> Type[Self]:
+        return type(
+            f"{cls.__name__}",
+            cls.__bases__,
+            dict(cls.__dict__) | dict(_alternative_name=alternative_name),
+        )
+
+
+def AddInterface(cls_name, cls_parents, cls_attrs):
+    @property
+    def base_property(base_self) -> Add:
+        @property
+        def value_property(self) -> int:
+            return getattr(base_self, base_self._alternative_name)
+
+        return type(cls_name, (), {"value": value_property})()
+
+    return type(cls_name, cls_parents + (Interface,), cls_attrs | {"Add": base_property})
+
+
+class Add(metaclass=AddInterface):
     value: int
 
     def __add__(self, other: Add) -> Self:
-        return type(self)(self.value + other.value)
+        return type(self)(self.Add.value + other.Add.value)
