c13473f8ed058279b748669bcf1f3c296557fe07
George <george-ogden@outlook.com>
2024-04-30 10:54:03 +0100

    remove previous, specific interface

--- interface.py
+++ interface.py
@@ -3,31 +3,6 @@ from __future__ import annotations
 from typing import Dict, Self, Tuple, Type, Union
 
 
-class AddInterface:
-    _alternative_name: str = "value"
-
-    def __class_getitem__(cls, alternative_name: str) -> Type[Self]:
-        return type(
-            f"{cls.__name__}",
-            cls.__bases__,
-            dict(cls.__dict__) | dict(_alternative_name=alternative_name),
-        )
-
-
-def AddMeta(cls_name, cls_parents, cls_attrs):
-    @property
-    def base_property(base_self) -> Add:
-        def value_getter(self) -> int:
-            return getattr(base_self, base_self._alternative_name)
-
-        def value_setter(self, value) -> int:
-            return setattr(base_self, base_self._alternative_name, value)
-
-        return type(cls_name, (), {"value": property(value_getter, value_setter)})()
-
-    return type(cls_name, cls_parents + (AddInterface,), cls_attrs | {"Add": base_property})
-
-
 def InterfaceMeta(cls_name, cls_parents, cls_attrs):
     # * Make any annotations or defined variables public.
     # Assume all methods have annotations.
