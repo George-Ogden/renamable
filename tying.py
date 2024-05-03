d355672f66083c761553039465a4862995979bf8
George <george-ogden@outlook.com>
2024-05-03 15:52:24 +0100

    rewrite with separate decorator and base

--- tying.py
+++ tying.py
@@ -2,24 +2,30 @@ from __future__ import annotations
 
 import inspect
 import itertools
-from typing import Any, Dict, Type
+from typing import Any, ClassVar, Dict, Set, Type
 
 
-class Renamable:
-    __attribute_names: Dict[str, str]
+class renamable:
+    """Decorator to make a class renamable."""
 
     def __new__(mcls, cls: Type[Renamable]) -> Type[Renamable]:
+        if not Renamable in cls.mro():
+            bases = cls.__bases__
+            if bases == (object,):
+                bases = ()
+            cls = type(
+                cls.__name__,
+                bases + (Renamable,),
+                dict(cls.__dict__),
+            )
         cls = mcls._rename_attributes(cls)
         cls = mcls._add_property(cls)
-        cls = mcls._add_class_getitem(cls)
         return cls
 
     @classmethod
     def _rename_attributes(mcls, cls: Type[Renamable]) -> Type[Renamable]:
         attributes_names = set(itertools.chain(vars(cls), inspect.get_annotations(cls)))
-        cls.__attribute_names = {
-            name: name for name in attributes_names if not name.startswith("_")
-        }
+        cls._renamable_attributes = {name for name in attributes_names if not name.startswith("_")}
         return cls
 
     @classmethod
@@ -29,17 +35,11 @@ class Renamable:
         @property
         def base_property(derived_self):
             public_properties: Dict[str, property] = {}
-            for name in cls.__attribute_names:
+            for name in cls._renamable_attributes:
                 public_properties[name] = mcls._make_property(derived_self, name, settable=True)
             return type(cls.__name__, (), public_properties)()
 
-        def lookup_variable(self: Renamable, name: str) -> str:
-            while name in self.__attribute_names and self.__attribute_names[name] != name:
-                name = self.__attribute_names[name]
-            return name
-
         setattr(cls, cls.__name__, base_property)
-        setattr(cls, "_lookup_variable", lookup_variable)
         return cls
 
     @classmethod
@@ -57,22 +57,32 @@ class Renamable:
         else:
             return property(getter)
 
-    @classmethod
-    def _add_class_getitem(mcls, cls: Type[Renamable]) -> Type[Renamable]:
-        def class_getitem(
-            cls: Type[Renamable], alternative_names: Dict[str, Any]
-        ) -> Type[Renamable]:
-            # Copy class.
-            cls = type(cls.__name__, cls.__bases__, dict(cls.__dict__))
-            for k, v in alternative_names.items():
-                if k not in cls.__attribute_names:
-                    raise ValueError(
-                        f"`{k}` not found in {cls.__name__} but trying to rename to `{v}`"
-                    )
-                cls.__attribute_names[k] = v
-                setattr(cls, v, getattr(cls, k))
-                delattr(cls, k)
-            return cls
 
-        cls.__class_getitem__ = class_getitem.__get__(cls)
+class Renamable:
+    _alternative_names: ClassVar[Dict[str, str]] = dict()
+    _renamable_attributes: ClassVar[Set[str]] = set()
+
+    def __class_getitem__(
+        cls: Type[Renamable], alternative_names: Dict[str, Any]
+    ) -> Type[Renamable]:
+        # Copy class.
+        name_suffix = f'[{",".join(f"{k}={v}" for k,v in alternative_names.items())}]'
+        cls = type(cls.__name__ + name_suffix, cls.__bases__, dict(cls.__dict__))
+        cls._alternative_names = dict(cls._alternative_names)
+        cls._renamable_attributes = set(cls._renamable_attributes)
+
+        for k, v in alternative_names.items():
+            if k not in cls._renamable_attributes:
+                raise ValueError(
+                    f"`{k}` not allowed to be renamed in {cls.__name__} but trying to rename to `{v}`"
+                )
+            cls._alternative_names[k] = v
+            cls._renamable_attributes.remove(k)
+            setattr(cls, v, getattr(cls, k))
+            delattr(cls, k)
         return cls
+
+    def _lookup_variable(self, name: str) -> str:
+        while name in self._alternative_names and self._alternative_names[name] != name:
+            name = self._alternative_names[name]
+        return name
