fc02719037700a73fda995f48438028056d20568
George <george-ogden@outlook.com>
2024-05-03 17:00:45 +0100

    test renaming methods

---
+++ tying_test.py
@@ -0,0 +1,133 @@
+import pytest
+
+from tying import renamable
+
+
+def test_single_inheritance():
+    @renamable
+    class Foo:
+        def bar(self) -> str:
+            return "bar"
+
+    class FooChild(Foo[dict(bar="bar_child")]):
+        def bar_child(self) -> str:
+            return "bar_child"
+
+    foo = FooChild()
+    assert foo.bar_child() == "bar_child"
+
+    with pytest.raises(AttributeError):
+        foo.bar()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_child()
+
+    assert foo.Foo.bar() == "bar_child"
+
+
+def test_multiple_inheritance():
+    @renamable
+    class Foo:
+        def bar(self) -> str:
+            return "bar"
+
+    class FooChild(Foo[dict(bar="bar_child")]):
+        def bar_child(self) -> str:
+            return "bar_child"
+
+    class FooGrandChild(FooChild):
+        def bar_child(self) -> str:
+            return "bar_child"
+
+    foo = FooGrandChild()
+    assert foo.bar_child() == "bar_child"
+
+    with pytest.raises(AttributeError):
+        foo.bar()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_child()
+
+    assert foo.Foo.bar() == "bar_child"
+
+
+def test_multiple_inheritance_overwrite_parent():
+    @renamable
+    class Foo:
+        def bar(self) -> str:
+            return "bar"
+
+    @renamable
+    class FooChild(Foo[dict(bar="bar_child")]):
+        def bar_child(self) -> str:
+            return "bar_child"
+
+    class FooGrandChild(FooChild[dict(bar_child="bar_grandchild")]):
+        def bar_grandchild(self) -> str:
+            return "bar_grandchild"
+
+    foo = FooChild()
+    with pytest.raises(AttributeError):
+        assert foo.bar_grandchild()
+
+    foo.bar_child() == "bar_child"
+
+    with pytest.raises(AttributeError):
+        foo.bar()
+
+    with pytest.raises(AttributeError):
+        foo.FooChild.bar_grandchild()
+
+    assert foo.FooChild.bar_child() == "bar_child"
+
+    with pytest.raises(AttributeError):
+        foo.FooChild.bar()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_grandchild()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_child()
+
+    assert foo.Foo.bar() == "bar_child"
+
+
+def test_multiple_inheritance_overwrite_child():
+    @renamable
+    class Foo:
+        def bar(self) -> str:
+            return "bar"
+
+    @renamable
+    class FooChild(Foo[dict(bar="bar_child")]):
+        def bar_child(self) -> str:
+            return "bar_child"
+
+    class FooGrandChild(FooChild[dict(bar_child="bar_grandchild")]):
+        def bar_grandchild(self) -> str:
+            return "bar_grandchild"
+
+    foo = FooGrandChild()
+    assert foo.bar_grandchild() == "bar_grandchild"
+
+    with pytest.raises(AttributeError):
+        foo.bar_child()
+
+    with pytest.raises(AttributeError):
+        foo.bar()
+
+    with pytest.raises(AttributeError):
+        foo.FooChild.bar_grandchild()
+
+    assert foo.FooChild.bar_child() == "bar_grandchild"
+
+    with pytest.raises(AttributeError):
+        foo.FooChild.bar()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_grandchild()
+
+    with pytest.raises(AttributeError):
+        foo.Foo.bar_child()
+
+    assert foo.Foo.bar() == "bar_grandchild"
